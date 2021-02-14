9338acff0f0d83169d7f8e797a69dab35ecbf6e1
cfgabi <cfgabi@hotmail.com>
2021-02-13 18:16:37 -0800

    Instalação e Configuração da gem Devise; Criação de controller Home para página inicial.
    Foi adicionada a gem Devise ao gemfile para gerenciamento de usuários e autenticação da aplicação.
    Criação de controller Home para funcionar como página inicial da aplicação. A rota padrão da aplicação foi configurada para acessar a página index do controller Home.
    Foram geradas as tabelas referentes aos Usuários utilizando as funções da gem Devise.
    Foi feita a migração do banco de dados para a aplicação.

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 Este repositório foi criado para o desenvolvimento de um projeto de aplicativo simples para pedidos em restaurantes. Esse projeto é parte das atividades do curso "Da arquitetura ao deploy com Ruby", da Digital Innovation One.
 
+
+
+@cfgabi
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # O Home/index foi criado com o intuito de ser a página inicial da aplicação
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
