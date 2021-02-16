81fdba670b7aa22f0d60097aa18d01c8f2f41dd7
cfgabi <cfgabi@hotmail.com>
2021-02-15 16:28:26 -0800

    Criação e Configuração de Controller para Produtos
    Foi criado um controller para os produtos da aplicação.
    Junto com o controller, foram definidos métodos new e create para a criação de novos produtos.
    Foram criadas páginas HTML com formulários para a criação de novos produtos utilizando os métodos criados.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # A criação do controller products gerou um método get product/index,
+  # porém, parece ser mais interessante tratar o controller products
+  # como um recurso na aplicação, porém com uma rota apenas para o index desse recurso.
+  # Depois foram criados os métodos new e create no controller products.
+  resources :products, only: [:index, :new, :create]
   resources :restaurants
   devise_for :users
   # O Home/index foi criado com o intuito de ser a página inicial da aplicação
